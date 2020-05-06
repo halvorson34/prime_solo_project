fb551b3af16390dc5fbf6033278f21e0d03500b2
Matt Halvorson <halvorson34.mh@gmail.com>
2020-05-06 18:10:43 -0500

    added frame for nav links. generic

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -19,28 +19,35 @@ const Nav = (props) => {
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
+        <h2 className="nav-title">Big Hearts</h2>
       </Link>
-      <div className="nav-right">
-        <Link className="nav-link" to={loginLinkData.path}>
-          {/* Show this link if they are logged in or not,
+      <div className="nav-middle">
+        <Link to="/">Feedback</Link>
+        <Link to="/">Doggo's</Link>
+        <Link to="/">Tasks</Link>
+        <Link to="/">Profile</Link>
+        <Link to="/">Home</Link>
+        <div className="nav-right">
+          <Link className="nav-link" to={loginLinkData.path}>
+            {/* Show this link if they are logged in or not,
           but call this link 'Home' if they are logged in,
           and call this link 'Login / Register' if they are not */}
-          {loginLinkData.text}
-        </Link>
-        {/* Show the link to the info page and the logout button if the user is logged in */}
-        {props.store.user.id && (
-          <>
-            <Link className="nav-link" to="/info">
-              Info Page
-            </Link>
-            <LogOutButton className="nav-link" />
-          </>
-        )}
-        {/* Always show this link since the about page is not protected */}
-        <Link className="nav-link" to="/about">
-          About
-        </Link>
+            {loginLinkData.text}
+          </Link>
+          {/* Show the link to the info page and the logout button if the user is logged in */}
+          {props.store.user.id && (
+            <>
+              <Link className="nav-link" to="/info">
+                Info Page
+              </Link>
+              <LogOutButton className="nav-link" />
+            </>
+          )}
+          {/* Always show this link since the about page is not protected */}
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+        </div>
       </div>
     </div>
   );
