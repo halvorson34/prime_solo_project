240238ff5047b3767733a82f660b2f1bba25b1a8
Matt Halvorson <halvorson34.mh@gmail.com>
2020-05-06 17:44:18 -0500

    created develop branch

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
-import mapStoreToProps from '../../redux/mapStoreToProps';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import "./Nav.css";
+import mapStoreToProps from "../../redux/mapStoreToProps";
 
 const Nav = (props) => {
   let loginLinkData = {
-    path: '/home',
-    text: 'Login / Register',
+    path: "/home",
+    text: "Login / Register",
   };
 
   if (props.store.user.id != null) {
-    loginLinkData.path = '/admin';
-    loginLinkData.text = 'Home';
+    loginLinkData.path = "/admin";
+    loginLinkData.text = "Home";
   }
 
   return (
@@ -34,7 +34,7 @@ const Nav = (props) => {
             <Link className="nav-link" to="/info">
               Info Page
             </Link>
-            <LogOutButton className="nav-link"/>
+            <LogOutButton className="nav-link" />
           </>
         )}
         {/* Always show this link since the about page is not protected */}
