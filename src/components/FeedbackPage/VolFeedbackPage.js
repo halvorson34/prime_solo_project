9c65b0e004cf244ace3cc5d019de72c4254929d3
Matt Halvorson <halvorson34.mh@gmail.com>
2020-05-09 20:23:18 -0500

    set up onChange and Onclick events for Issues and Thank Yous to be sent to feedback table in prime_app database

--- src/components/FeedbackPage/VolFeedbackPage.js
+++ src/components/FeedbackPage/VolFeedbackPage.js
@@ -4,8 +4,11 @@ import { connect } from "react-redux";
 class VolFeedbackPage extends Component {
   state = {
     comments: "",
+    issues: "",
+    thank_yous: "",
   };
 
+  // input on change for Comments
   addComments = (event) => {
     console.log("POST");
     this.setState({
@@ -13,7 +16,42 @@ class VolFeedbackPage extends Component {
     });
   };
 
+  // onClick event for Comments, send input data to feedback reducer and post to feedback table in prime_app database
   saveComments = (event) => {
+    event.preventDefault();
+    this.props.dispatch({
+      type: "SAVE_FEEDBACK",
+      payload: this.state,
+    });
+  };
+
+  // input on change for Issues
+  addIssues = (event) => {
+    console.log("POST");
+    this.setState({
+      issues: event.target.value,
+    });
+  };
+
+  // onClick event for Issues, send input data to feedback reducer and post to feedback table in prime_app database
+  saveIssues = (event) => {
+    event.preventDefault();
+    this.props.dispatch({
+      type: "SAVE_FEEDBACK",
+      payload: this.state,
+    });
+  };
+
+  // input on change for Thank Yous
+  addThankYous = (event) => {
+    console.log("POST");
+    this.setState({
+      thank_yous: event.target.value,
+    });
+  };
+
+  // onClick event for Thank Yous, send input data to feedback reducer and post to feedback table in prime_app database
+  saveThankYous = (event) => {
     event.preventDefault();
     this.props.dispatch({
       type: "SAVE_FEEDBACK",
@@ -28,6 +66,10 @@ class VolFeedbackPage extends Component {
         <form>
           <input onChange={this.addComments} type="text" />
           <button onClick={this.saveComments}>Submit</button>
+          <input onChange={this.addIssues} type="text" />
+          <button onClick={this.saveIssues}>Submit</button>
+          <input onChange={this.addThankYous} type="text" />
+          <button onClick={this.saveThankYous}>Submit</button>
         </form>
       </div>
     );
